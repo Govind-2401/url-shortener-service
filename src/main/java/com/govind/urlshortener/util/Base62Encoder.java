cfb06d443b24123cdf183896278e0a9bb1bef00c
Govind Kumar <govindkumar.gpj@gmail.com>
2026-07-21 14:54:39 +0530

    feat: add Base62 encoder utility for short code generation

---
+++ src/main/java/com/govind/urlshortener/util/Base62Encoder.java
@@ -0,0 +1,22 @@
+package com.govind.urlshortener.util;
+
+public class Base62Encoder {
+
+    private static final String BASE62 = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+    public static String encode(long value) {
+        if (value == 0) {
+            return String.valueOf(BASE62.charAt(0));
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        while (value > 0) {
+            int remainder = (int) (value % 62);
+            sb.append(BASE62.charAt(remainder));
+            value /= 62;
+        }
+
+        return sb.reverse().toString();
+    }
+}
